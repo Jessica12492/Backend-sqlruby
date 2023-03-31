30a6340b632b6a5f4d48dc456070278063d4aa65
Jessica <jessica12492@gmail.com>
2023-03-31 14:20:38 -0500

    made changes to sqlruby lesson 1.2

--- years_to_hours.rb
+++ years_to_hours.rb
@@ -5,13 +5,13 @@ hours = years * 365 * 24
 puts "That's #{hours} hours."
 
 puts "  Enter number of decades"
-decades= gets.chomp.to_i # this returns a string
-minutes= decades * 365 * 24*60
-puts " Thats #{minutes}minutes."
+decades = gets.chomp.to_i # this returns a string
+minutes = decades * 10 * 365 * 24 * 60
+puts " Thats #{minutes} minutes."
 
 puts "What is your age?"
 age = gets.chomp.to_i
-seconds = age * 365 *24*60*60
+seconds = age * 365 * 24 * 60 * 60
 puts "WOW! You are #{seconds} seconds old!"
 
 
